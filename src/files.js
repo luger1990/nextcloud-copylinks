2c92abf0bd8a02df6138404aeb90b63bf2a568d1
luger <luger1990@gmail.com>
2025-07-25 16:03:52 +0800

    add image/video/audio export

--- src/files.js
+++ src/files.js
@@ -73,12 +73,135 @@ registerFileListAction({
         return '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg t="1751359290092" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5310" xmlns:xlink="http://www.w3.org/1999/xlink" width="1024" height="1024"><path d="M749 205.7H293.7l-230.1 230V891c0 37.8 30.7 68.5 68.5 68.5H749c37.8 0 68.5-30.7 68.5-68.5V274.2c0.1-37.8-30.7-68.5-68.5-68.5zM276.1 322v95.7l-95.7-1.1 95.7-94.6zM132.2 891V484.6l177.8 2.1h0.4c9 0 17.7-3.6 24.1-9.9 6.5-6.4 10.2-15.2 10.2-24.4V274.2H749V891H132.2z" fill="#2C2C2C" p-id="5311"></path><path d="M639.4 726.5H235c-18.9 0-34.3 15.3-34.3 34.3 0 18.9 15.3 34.3 34.3 34.3h404.4c18.9 0 34.3-15.3 34.3-34.3-0.1-18.9-15.4-34.3-34.3-34.3zM639.4 418.1H426.9c-18.9 0-34.3 15.3-34.3 34.3 0 18.9 15.3 34.3 34.3 34.3h212.5c18.9 0 34.3-15.3 34.3-34.3-0.1-18.9-15.4-34.3-34.3-34.3zM639.4 575.8H235c-18.9 0-34.3 15.3-34.3 34.3s15.3 34.3 34.3 34.3h404.4c18.9 0 34.3-15.3 34.3-34.3s-15.4-34.3-34.3-34.3zM925.1 63.9H465.9c-18.9 0-34.3 15.3-34.3 34.3s15.3 34.3 34.3 34.3h424.9v562c0 18.9 15.4 34.3 34.3 34.3s34.3-15.3 34.3-34.3V98.1c-0.1-18.9-15.4-34.2-34.3-34.2z" fill="#2C2C2C" p-id="5312"></path></svg>';
     },
     id: 'CopySelectedLinks',
+    order: 6
+})
+
+registerFileListAction({
+    displayName: function () {
+        return 'Image_QualityCheck';
+    },
+    enabled: function () {
+        return true;
+    },
+    exec: async function () {
+        try {
+                const response = await fetch('/export_image', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ path: currentPath })
+                });
+                
+                const data = await response.json();
+                
+                if (data.status === 'success') {
+                    // 下载文件
+                    window.location.href = `/download/${encodeURIComponent(data.file_path)}?filename=${encodeURIComponent(data.file_name)}`;
+                    showSuccess("开始下载文件", {"timeout": 1000})
+                } else {
+                    showError("导出Excel时出错,请联系Luger", {"timeout": 1000})
+                }
+            } catch (err) {
+                console.error(err);
+                showError("导出Excel时出错,请联系Luger" + err.message, {"timeout": 1000})
+            } finally {
+                
+            }
+        
+    },
+    iconSvgInline: function () {
+        return '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg t="1751359560030" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="8850" xmlns:xlink="http://www.w3.org/1999/xlink" width="1024" height="1024"><path d="M394.6 689.8c-13.2 3.2-27.4-9.5-31.7-23.2-8.4-28.5-12.7-58.6-12.7-88.1 0-159.4 151.5-325.1 314.1-352.1v-5.3c0-1.6 0-3.7 0.5-5.3l0.5-2.1c1.6-11.6 5.8-38.5 32.2-55.4 10.6-6.9 22.2-10 34.3-10 14.8 0 26.9 5.3 34.3 9.5 1.6 0.5 2.6 1.6 4.2 2.6L929.2 286c17.4 13.2 28 34.8 28 57 0 22.7-10.6 44.3-28.5 57.5L770.5 525.7c-11.6 9-25.3 13.7-39.1 13.7-12.1 0-23.8-3.7-34.8-10.6-21.1-13.7-31.7-34.8-31.7-64.4v-1.1C559.3 475 442.2 569 416.3 657.6c-4.8 14.3-13.7 32.2-21.7 32.2z m335.7-466.6V256c0 17.4-13.7 31.7-31.1 32.7-125.6 6.9-257.6 131.4-279.7 254.4C486 458.7 595.2 390 699.7 397.4c17.4 1.1 30.6 15.3 30.6 32.7V465c0 4.7 0.5 6.9 0.5 7.9l157.3-124.6c2.1-1.6 2.6-3.2 2.6-5.3 0-2.1-1.1-4.2-2.6-5.3L731.9 214.3c-1.1 1.6-1.6 5.8-2.1 8.4l0.5 0.5z m0 0" p-id="8851"></path><path d="M715.6 916.3H188.3c-67 0-121.9-54.9-121.9-121.9v-568c0-67 54.9-121.9 121.9-121.9h436.5v69.7H188.3c-28.5 0-52.3 23.2-52.3 52.3V795c0 28.5 23.2 52.3 52.3 52.3h527.3c28.5 0 52.3-23.2 52.3-52.3V593.2H837v201.6c0 66.6-54.4 121.5-121.4 121.5z m0 0" p-id="8852"></path><path d="M183 566.9h112.4V626H183v-59.1z m0 154.1h296.1v59.1H183V721z m0-425.4h237v59.1H183v-59.1z m0 140.9h112.4v59.1H183v-59.1z m0 0" p-id="8853"></path></svg>';
+    },
+    id: 'Image_QualityCheck',
+    order: 5
+})
+
+registerFileListAction({
+    displayName: function () {
+        return 'Audio_QualityCheck';
+    },
+    enabled: function () {
+        return true;
+    },
+    exec: async function () {
+        try {
+                const response = await fetch('/export_audio', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ path: currentPath })
+                });
+                
+                const data = await response.json();
+                
+                if (data.status === 'success') {
+                    // 下载文件
+                    window.location.href = `/download/${encodeURIComponent(data.file_path)}?filename=${encodeURIComponent(data.file_name)}`;
+                    showSuccess("开始下载文件", {"timeout": 1000})
+                } else {
+                    showError("导出Excel时出错,请联系Luger", {"timeout": 1000})
+                }
+            } catch (err) {
+                console.error(err);
+                showError("导出Excel时出错,请联系Luger" + err.message, {"timeout": 1000})
+            } finally {
+                
+            }
+        
+    },
+    iconSvgInline: function () {
+        return '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg t="1751359560030" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="8850" xmlns:xlink="http://www.w3.org/1999/xlink" width="1024" height="1024"><path d="M394.6 689.8c-13.2 3.2-27.4-9.5-31.7-23.2-8.4-28.5-12.7-58.6-12.7-88.1 0-159.4 151.5-325.1 314.1-352.1v-5.3c0-1.6 0-3.7 0.5-5.3l0.5-2.1c1.6-11.6 5.8-38.5 32.2-55.4 10.6-6.9 22.2-10 34.3-10 14.8 0 26.9 5.3 34.3 9.5 1.6 0.5 2.6 1.6 4.2 2.6L929.2 286c17.4 13.2 28 34.8 28 57 0 22.7-10.6 44.3-28.5 57.5L770.5 525.7c-11.6 9-25.3 13.7-39.1 13.7-12.1 0-23.8-3.7-34.8-10.6-21.1-13.7-31.7-34.8-31.7-64.4v-1.1C559.3 475 442.2 569 416.3 657.6c-4.8 14.3-13.7 32.2-21.7 32.2z m335.7-466.6V256c0 17.4-13.7 31.7-31.1 32.7-125.6 6.9-257.6 131.4-279.7 254.4C486 458.7 595.2 390 699.7 397.4c17.4 1.1 30.6 15.3 30.6 32.7V465c0 4.7 0.5 6.9 0.5 7.9l157.3-124.6c2.1-1.6 2.6-3.2 2.6-5.3 0-2.1-1.1-4.2-2.6-5.3L731.9 214.3c-1.1 1.6-1.6 5.8-2.1 8.4l0.5 0.5z m0 0" p-id="8851"></path><path d="M715.6 916.3H188.3c-67 0-121.9-54.9-121.9-121.9v-568c0-67 54.9-121.9 121.9-121.9h436.5v69.7H188.3c-28.5 0-52.3 23.2-52.3 52.3V795c0 28.5 23.2 52.3 52.3 52.3h527.3c28.5 0 52.3-23.2 52.3-52.3V593.2H837v201.6c0 66.6-54.4 121.5-121.4 121.5z m0 0" p-id="8852"></path><path d="M183 566.9h112.4V626H183v-59.1z m0 154.1h296.1v59.1H183V721z m0-425.4h237v59.1H183v-59.1z m0 140.9h112.4v59.1H183v-59.1z m0 0" p-id="8853"></path></svg>';
+    },
+    id: 'Audio_QualityCheck',
+    order: 4
+})
+
+registerFileListAction({
+    displayName: function () {
+        return 'Video_QualityCheck';
+    },
+    enabled: function () {
+        return true;
+    },
+    exec: async function () {
+        try {
+                const response = await fetch('/export_video', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ path: currentPath })
+                });
+                
+                const data = await response.json();
+                
+                if (data.status === 'success') {
+                    // 下载文件
+                    window.location.href = `/download/${encodeURIComponent(data.file_path)}?filename=${encodeURIComponent(data.file_name)}`;
+                    showSuccess("开始下载文件", {"timeout": 1000})
+                } else {
+                    showError("导出Excel时出错,请联系Luger", {"timeout": 1000})
+                }
+            } catch (err) {
+                console.error(err);
+                showError("导出Excel时出错,请联系Luger" + err.message, {"timeout": 1000})
+            } finally {
+                
+            }
+        
+    },
+    iconSvgInline: function () {
+        return '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg t="1751359560030" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="8850" xmlns:xlink="http://www.w3.org/1999/xlink" width="1024" height="1024"><path d="M394.6 689.8c-13.2 3.2-27.4-9.5-31.7-23.2-8.4-28.5-12.7-58.6-12.7-88.1 0-159.4 151.5-325.1 314.1-352.1v-5.3c0-1.6 0-3.7 0.5-5.3l0.5-2.1c1.6-11.6 5.8-38.5 32.2-55.4 10.6-6.9 22.2-10 34.3-10 14.8 0 26.9 5.3 34.3 9.5 1.6 0.5 2.6 1.6 4.2 2.6L929.2 286c17.4 13.2 28 34.8 28 57 0 22.7-10.6 44.3-28.5 57.5L770.5 525.7c-11.6 9-25.3 13.7-39.1 13.7-12.1 0-23.8-3.7-34.8-10.6-21.1-13.7-31.7-34.8-31.7-64.4v-1.1C559.3 475 442.2 569 416.3 657.6c-4.8 14.3-13.7 32.2-21.7 32.2z m335.7-466.6V256c0 17.4-13.7 31.7-31.1 32.7-125.6 6.9-257.6 131.4-279.7 254.4C486 458.7 595.2 390 699.7 397.4c17.4 1.1 30.6 15.3 30.6 32.7V465c0 4.7 0.5 6.9 0.5 7.9l157.3-124.6c2.1-1.6 2.6-3.2 2.6-5.3 0-2.1-1.1-4.2-2.6-5.3L731.9 214.3c-1.1 1.6-1.6 5.8-2.1 8.4l0.5 0.5z m0 0" p-id="8851"></path><path d="M715.6 916.3H188.3c-67 0-121.9-54.9-121.9-121.9v-568c0-67 54.9-121.9 121.9-121.9h436.5v69.7H188.3c-28.5 0-52.3 23.2-52.3 52.3V795c0 28.5 23.2 52.3 52.3 52.3h527.3c28.5 0 52.3-23.2 52.3-52.3V593.2H837v201.6c0 66.6-54.4 121.5-121.4 121.5z m0 0" p-id="8852"></path><path d="M183 566.9h112.4V626H183v-59.1z m0 154.1h296.1v59.1H183V721z m0-425.4h237v59.1H183v-59.1z m0 140.9h112.4v59.1H183v-59.1z m0 0" p-id="8853"></path></svg>';
+    },
+    id: 'Video_QualityCheck',
     order: 3
 })
 
 registerFileListAction({
     displayName: function () {
-        return 'ExportMeta';
+        return 'Filename Robot';
     },
     enabled: function () {
         return true;
@@ -113,7 +236,7 @@ registerFileListAction({
     iconSvgInline: function () {
         return '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg t="1751359560030" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="8850" xmlns:xlink="http://www.w3.org/1999/xlink" width="1024" height="1024"><path d="M394.6 689.8c-13.2 3.2-27.4-9.5-31.7-23.2-8.4-28.5-12.7-58.6-12.7-88.1 0-159.4 151.5-325.1 314.1-352.1v-5.3c0-1.6 0-3.7 0.5-5.3l0.5-2.1c1.6-11.6 5.8-38.5 32.2-55.4 10.6-6.9 22.2-10 34.3-10 14.8 0 26.9 5.3 34.3 9.5 1.6 0.5 2.6 1.6 4.2 2.6L929.2 286c17.4 13.2 28 34.8 28 57 0 22.7-10.6 44.3-28.5 57.5L770.5 525.7c-11.6 9-25.3 13.7-39.1 13.7-12.1 0-23.8-3.7-34.8-10.6-21.1-13.7-31.7-34.8-31.7-64.4v-1.1C559.3 475 442.2 569 416.3 657.6c-4.8 14.3-13.7 32.2-21.7 32.2z m335.7-466.6V256c0 17.4-13.7 31.7-31.1 32.7-125.6 6.9-257.6 131.4-279.7 254.4C486 458.7 595.2 390 699.7 397.4c17.4 1.1 30.6 15.3 30.6 32.7V465c0 4.7 0.5 6.9 0.5 7.9l157.3-124.6c2.1-1.6 2.6-3.2 2.6-5.3 0-2.1-1.1-4.2-2.6-5.3L731.9 214.3c-1.1 1.6-1.6 5.8-2.1 8.4l0.5 0.5z m0 0" p-id="8851"></path><path d="M715.6 916.3H188.3c-67 0-121.9-54.9-121.9-121.9v-568c0-67 54.9-121.9 121.9-121.9h436.5v69.7H188.3c-28.5 0-52.3 23.2-52.3 52.3V795c0 28.5 23.2 52.3 52.3 52.3h527.3c28.5 0 52.3-23.2 52.3-52.3V593.2H837v201.6c0 66.6-54.4 121.5-121.4 121.5z m0 0" p-id="8852"></path><path d="M183 566.9h112.4V626H183v-59.1z m0 154.1h296.1v59.1H183V721z m0-425.4h237v59.1H183v-59.1z m0 140.9h112.4v59.1H183v-59.1z m0 0" p-id="8853"></path></svg>';
     },
-    id: 'ExportMeta',
+    id: 'FileNameRobot',
     order: 2
 })
 
